b40ad0357d0db83f0f0966359591bf6519c0cf81
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-03-04 15:20:27 +0330

    commit message

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -202,6 +202,13 @@ tab6.forEach((tab) => {
 // modal
 
 let closeModal=Array.from(document.getElementsByClassName('close-button'));
+let showNotif=document.querySelector('.showNotif');
+let notificationModalBox=document.querySelector('.notification-modalBox');
+
+showNotif.addEventListener('click',function () {
+  notificationModalBox.classList.add('d-block');
+  notificationModalBox.querySelector('.modal-container').classList.remove('active');
+})
 
 closeModal.forEach((item)=>{
   item.addEventListener('click',function () {
