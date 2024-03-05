04d55c965acf95d6a264b239ab9dfe94dab83c89
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-03-05 18:56:09 +0330

    commit message

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -234,20 +234,3 @@ tab6.forEach((tab) => {
 
 jalaliDatepicker.startWatch();
 
-// modal
-
-let closeModal=Array.from(document.getElementsByClassName('close-button'));
-let showNotif=document.querySelector('.showNotif');
-let notificationModalBox=document.querySelector('.notification-modalBox');
-
-showNotif.addEventListener('click',function () {
-  notificationModalBox.classList.add('d-block');
-  notificationModalBox.querySelector('.modal-container').classList.remove('active');
-})
-
-closeModal.forEach((item)=>{
-  item.addEventListener('click',function () {
-    item.parentElement.parentElement.classList.add('active');
-    item.parentElement.parentElement.parentElement.classList.remove('d-block')
-  })
-})
