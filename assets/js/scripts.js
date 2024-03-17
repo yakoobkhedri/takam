a746072b214a7516e023733050daafc059276b6c
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-03-17 21:54:23 +0330

    commit message

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -11,3 +11,25 @@ var login = new Swiper(".login", {
     el: ".swiper-pagination",
   },
 });
+// show password
+const togglePassword = document.querySelector("#togglePassword");
+const togglePassword2 = document.querySelector("#togglePassword2");
+const password = document.querySelector("#pass1");
+const password2 = document.querySelector("#pass2");
+
+togglePassword.addEventListener("click", function () {
+  // toggle the type attribute
+  const type = password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  
+  // toggle the icon
+  this.classList.toggle("bi-eye");
+});
+togglePassword2.addEventListener("click", function () {
+  // toggle the type attribute
+  const type = password2.getAttribute("type") === "password" ? "text" : "password";
+  password2.setAttribute("type", type);
+  
+  // toggle the icon
+  this.classList.toggle("bi-eye");
+});
